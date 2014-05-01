27f78422796630b57585b480fe001e2b47ae3b32
ytanruengsri <yuthasak.tanruengsri@googlemail.com>
2014-05-01 19:21:40 +0200

    create a minified version

--- GruntFile.js
+++ GruntFile.js
@@ -1,6 +1,9 @@
 module.exports = function (grunt) {
 
     grunt.loadNpmTasks('grunt-html2js');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.registerTask('compress', ['build', 'concat:js', 'uglify:js']);
     grunt.registerTask('build', ['html2js']);
 
     // Project configuration.
@@ -9,6 +12,21 @@ module.exports = function (grunt) {
         src: {
             js: ['*.js'],
             html: ['template/*.html']
+        },
+        concat: {
+            js : {
+                src : [
+                    'angular-*.js'
+                ],
+                dest : 'angular-notification.min.js'
+            }
+        },
+        uglify : {
+            js: {
+                files: {
+                    'angular-notification.min.js' : ['angular-notification.min.js']
+                }
+            }
         },
         html2js: {
             app: {
