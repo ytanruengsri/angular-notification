dca27d8107b7bcaf3db0e552500c2051a5ec664e
ytanruengsri <yuthasak.tanruengsri@googlemail.com>
2014-05-03 23:53:14 +0200

    Update module name

--- assets/angular-notification/angular-notification-template.js
+++ assets/angular-notification/angular-notification-template.js
@@ -3,7 +3,7 @@ angular.module('tellMeTemplate', ['../template/angular-notification-template.htm
 angular.module("../template/angular-notification-template.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("../template/angular-notification-template.html",
     "<div class=\"y-notication {{position}}\">" +
-          "<div ng-repeat=\"msg in messages\" ng-click=\"hideMessage(msg)\">\n" +
+          "<div class=\"y-notification-wrapper\" ng-repeat=\"msg in messages\" ng-click=\"hideMessage(msg)\">\n" +
           "     <div class=\"y-notification-msg {{msg.level}}\">\n" +
           "         <div class=\"y-notification-body\" style=\"padding: 10px 20px;\">\n" +
           "             <div class=\"y-notification-title\">{{msg.title}}</div>\n" +
